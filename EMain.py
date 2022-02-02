0f21284f2bd7606f7edaaabdfba5733e497d73b3
spikal2000 <spikal1997@gmail.com>
2022-02-02 16:00:59 +0200

    mySQL Db implementation
    implement Create db and table in main program

--- EMain.py
+++ EMain.py
@@ -3,6 +3,7 @@ import csv
 import tkinter as tk
 from tkinter import filedialog
 from datetime import datetime
+import mysql.connector
 
 #Asking the user for file
 root = tk.Tk()
@@ -87,7 +88,7 @@ for (i,g,k) in zip(products, quantities, values):
     print(i,g,k)
 
 
-#----parse string to float number before insert the values in mysql DB----
+#----parse strings to float numbers and date to DateTime before insert the values in mysql DB----
 #print(float(money[0].replace(',','.')))
 datedtr = " ".join(x)
 date_time = datetime.strptime(datedtr, '%d.%m.%Y %H:%M:%S')  # DATE
@@ -103,6 +104,52 @@ for quantity in quantities:
 values_num = list() # VALUES
 for value in values:
     values_num.append(float(value.replace(',', '.')))
+
+
+#_______________________DataBase Connection_______________________
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="dev123"
+)
+
+print(mydb)
+
+mycursor = mydb.cursor()
+
+mycursor.execute(" CREATE DATABASE IF NOT EXISTS `mainDB` ")
+
+mycursor.execute("""
+
+    CREATE TABLE IF NOT EXISTS `mainDB`.`products` (
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(500) NOT NULL,
+    `quantity` FLOAT NOT NULL,
+    `value` FLOAT NOT NULL,
+    PRIMARY KEY (`id`));
+
+""")
+
+mycursor.execute("""
+
+CREATE TABLE IF NOT EXISTS `mainDB`.`sales` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `branch` VARCHAR(500) NOT NULL,
+  `Date` DATETIME NOT NULL,
+  `income` FLOAT NOT NULL,
+  `expenses` FLOAT NOT NULL,
+  `cash` FLOAT NOT NULL,
+  `creditCard` FLOAT NOT NULL,
+  `product_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `FK_sales_products_id__products_id_idx` (`product_id` ASC) VISIBLE,
+  CONSTRAINT `FK_sales_products_id__products_id`
+    FOREIGN KEY (`product_id`)
+    REFERENCES `test`.`products` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+""")
 
 
 
