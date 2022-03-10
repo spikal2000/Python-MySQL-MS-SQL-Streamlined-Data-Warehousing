0f67787a3e9324c458612541df5bbcf1bf1ed14c
spikal2000 <spikal1997@gmail.com>
2022-03-10 15:07:18 +0200

    minor changes

--- MainProgram.py
+++ MainProgram.py
@@ -158,9 +158,9 @@ def start(branch, file_path):
 
     #_______________________DataBase Connection_______________________
     mydb = mysql.connector.connect(
-      host="ns1.eude1.armadaservers.com",
-      user="kalogeri_spyros",
-      password="vaggosspyros1997"
+      host="localhost",
+      user="root",
+      password="dev123"
     )
 
     print(mydb)
@@ -170,7 +170,7 @@ def start(branch, file_path):
 
     mycursor.execute("""
 
-        CREATE TABLE IF NOT EXISTS `kalogeri_maindb`.`products` (
+        CREATE TABLE IF NOT EXISTS `maindb`.`products` (
       `id` INT NOT NULL AUTO_INCREMENT,
       `name` VARCHAR(500) NULL,
       `quantity` FLOAT NULL,
@@ -182,7 +182,7 @@ def start(branch, file_path):
 
     mycursor.execute("""
 
-    CREATE TABLE IF NOT EXISTS `kalogeri_maindb`.`sales` (
+    CREATE TABLE IF NOT EXISTS `maindb`.`sales` (
       `id` INT NOT NULL AUTO_INCREMENT,
       `branch` VARCHAR(500) NULL,
       `date` DATETIME NULL,
@@ -199,19 +199,19 @@ def start(branch, file_path):
 
     mycursor.execute("""
 
-    CREATE TABLE IF NOT EXISTS `kalogeri_maindb`.`productDetails` (
+    CREATE TABLE IF NOT EXISTS `maindb`.`productDetails` (
       `sales_id` INT NOT NULL,
       `product_id` INT NOT NULL,
       INDEX `FK_sales_id__productDeatils_sales_id_idx` (`sales_id` ASC),
       INDEX `FK_products_id__productDetails_products_id_idx` (`product_id` ASC),
       CONSTRAINT `FK_sales_id__productDeatils_sales_id`
         FOREIGN KEY (`sales_id`)
-        REFERENCES `kalogeri_maindb`.`sales` (`id`)
+        REFERENCES `maindb`.`sales` (`id`)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION,
       CONSTRAINT `FK_products_id__productDetails_products_id`
         FOREIGN KEY (`product_id`)
-        REFERENCES `kalogeri_maindb`.`products` (`id`)
+        REFERENCES `maindb`.`products` (`id`)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION)
         ENGINE = InnoDB
@@ -223,20 +223,20 @@ def start(branch, file_path):
 
     mycursor.execute("""
 
-    CREATE TABLE IF NOT EXISTS `kalogeri_maindb`.`Dproduct_id` (
+    CREATE TABLE IF NOT EXISTS `maindb`.`Dproduct_id` (
       `product_id` INT NOT NULL);
 
     """)
 
     mycursor.execute("""
 
-    CREATE TABLE IF NOT EXISTS `kalogeri_maindb`.`Dsales_id` (
+    CREATE TABLE IF NOT EXISTS `maindb`.`Dsales_id` (
       `sales_id` INT NOT NULL);
 
     """)
 
     mycursor.execute("""
-        INSERT INTO `kalogeri_maindb`.`sales`(`branch`, `Date`, `income`, `expenses`,
+        INSERT INTO `maindb`.`sales`(`branch`, `Date`, `income`, `expenses`,
         `cash`, `creditCard`)
         VALUES(%s, %s, %s, %s, %s, %s)
     """, (branch, date_time, income, expenses, i_cash, credit_card))
@@ -246,7 +246,7 @@ def start(branch, file_path):
 
     mycursor.execute("""
 
-        INSERT INTO `kalogeri_maindb`.`Dsales_id`(`sales_id`)
+        INSERT INTO `maindb`.`Dsales_id`(`sales_id`)
         VALUES ( %s )
 
     """,(sales_id,))
@@ -259,14 +259,14 @@ def start(branch, file_path):
         value1 = values_num[i]
 
         mycursor.execute("""
-            INSERT INTO `kalogeri_maindb`.`products`(`name`, `quantity`, `value`)
+            INSERT INTO `maindb`.`products`(`name`, `quantity`, `value`)
             VALUES (%s, %s, %s); """, (name1, quantity1, value1) )
 
         mydb.commit()
         product_id = mycursor.lastrowid
         mycursor.execute("""
 
-            INSERT INTO `kalogeri_maindb`.`productDetails`(`sales_id`, `product_id`)
+            INSERT INTO `maindb`.`productDetails`(`sales_id`, `product_id`)
             VALUES ( %s, %s )
 
             """, (sales_id, product_id))
@@ -274,7 +274,7 @@ def start(branch, file_path):
 
         mycursor.execute("""
 
-            INSERT INTO `kalogeri_maindb`.`Dproduct_id`(`product_id`)
+            INSERT INTO `maindb`.`Dproduct_id`(`product_id`)
             VALUES ( %s )
 
         """,(product_id,))
